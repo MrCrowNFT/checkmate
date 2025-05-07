fbc1919cf8b70af1b99c7d00dbbf6dc9dbcea198
MrCrowNFT <mrcrownft@gmail.com>
2025-05-07 08:35:03 -0400

    .env

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -4,12 +4,12 @@ import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
 //todo import from .env
 const firebaseConfig = {
-  apiKey: "",
-  authDoman: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
 };
 
 //initialize firebase
