0081f431c066ba84d0ab24e8c05ce2ce6f350a3b
MrCrowNFT <mrcrownft@gmail.com>
2025-05-07 08:55:52 -0400

    firebase fix

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-//todo import from .env
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
