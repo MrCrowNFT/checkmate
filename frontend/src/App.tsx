d3d56f650998b814d91f19d15e2af1dee0b22aa5
MrCrowNFT <mrcrownft@gmail.com>
2025-05-15 08:55:18 -0400

    theme initializer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import Auth from "./components/auth";
 import Deployments from "./components/deployments";
 import { AuthProvider, useAuth } from "./contexts/auth-context";
+import { useInitializeTheme } from "./hooks";
 
 function AuthenticatedApp() {
+   useInitializeTheme();
   const { currentUser } = useAuth();
 
   return <div>{currentUser ? <Deployments /> : <Auth />}</div>;
--- frontend/src/hooks/use-theme.ts
+++ frontend/src/hooks/use-theme.ts
@@ -1,20 +1,59 @@
 import { create } from "zustand";
+import { useEffect } from "react";
 
 type ThemeState = {
   darkMode: boolean;
   toggleDarkMode: () => void;
+  initializeTheme: () => void;
 };
 
 export const useThemeStore = create<ThemeState>((set) => ({
   darkMode:
-    typeof localStorage !== "undefined"
-      ? localStorage.getItem("darkMode") === "true"
-      : window.matchMedia("(prefers-color-scheme: dark)").matches,
+    typeof window !== "undefined"
+      ? localStorage.getItem("darkMode") === "true" ||
+        (localStorage.getItem("darkMode") === null &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      : false,
+
   toggleDarkMode: () =>
     set((state) => {
       const newMode = !state.darkMode;
-      localStorage.setItem("darkMode", newMode.toString());
-      document.documentElement.classList.toggle("dark", newMode);
+      if (typeof window !== "undefined") {
+        localStorage.setItem("darkMode", newMode.toString());
+        if (newMode) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
       return { darkMode: newMode };
     }),
+
+  initializeTheme: () =>
+    set((state) => {
+      if (typeof window !== "undefined") {
+        const darkModeStored = localStorage.getItem("darkMode");
+        const prefersDark = window.matchMedia(
+          "(prefers-color-scheme: dark)"
+        ).matches;
+        const isDarkMode =
+          darkModeStored !== null ? darkModeStored === "true" : prefersDark;
+
+        if (isDarkMode) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+        return { darkMode: isDarkMode };
+      }
+      return state;
+    }),
 }));
+
+export const useInitializeTheme = () => {
+  const { initializeTheme } = useThemeStore();
+
+  useEffect(() => {
+    initializeTheme();
+  }, []);
+};
